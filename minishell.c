831aed29abf99bb02b4c76a9133988e7dd657772
Filipa Barros <fibarros@c3a8c1.42berlin.de>
2024-07-08 14:02:44 +0200

    nothing important, just to be able to switch branches

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: fibarros <fibarros@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 12:07:43 by keramos-          #+#    #+#             */
-/*   Updated: 2024/07/08 10:39:21 by fibarros         ###   ########.fr       */
+/*   Updated: 2024/07/08 11:56:21 by fibarros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,22 +48,3 @@ int	main(int argc, char **argv, char **env)
 	receive_msg(&msh);
 	return (0);
 }
-
-/*
-
-Structure t_msh:
-
-If t_msh contains fields that are arrays or pointers, and their sizes are not known 
-at compile time or they need to grow/shrink dynamically, you should allocate memory 
-dynamically for those fields.
-If t_msh only contains simple data types (like integers or fixed-size arrays) and 
-pointers to memory that is already managed elsewhere (like env), then you don't 
-necessarily need to allocate memory dynamically for the t_msh structure itself.
-Sub-Structures or Arrays in t_msh:
-
-If t_msh contains sub-structures or arrays that need dynamic memory, you should 
-allocate memory for them. For example, if t_msh has a field char **env that you
- need to copy from the env parameter, then you need to allocate memory for 
- the array and each string.
- 
- */
